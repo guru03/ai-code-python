781a99b48506084f712cc4ae72bf9caaa9d9a26c
Guru Shankar <guru03@outlook.com>
2026-05-29 02:20:42 +0530

    all files updated

--- ai_code/apps/angular/urls.py
+++ ai_code/apps/angular/urls.py
@@ -3,10 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import AngularViewSet, update_angular
 
 router = DefaultRouter()
-router.register(r'ngapi', AngularViewSet)
+router.register(r"angular", AngularViewSet, basename="angular")
+
 
 urlpatterns = [
-    path('', include(router.urls)),
-    # path('ngapi/update/<int:pk>/', update_angular, name='update_angular'),
-    path('ngapi/update/', update_angular, name='update_angular'),
+    path("", include(router.urls)),  # all CRUD endpoints come from here
 ]
